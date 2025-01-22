83658a54322a1d5ed9df9077ee4c4305fa22efa7
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2025-01-22 12:21:05 +1100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,6 @@ import pyvis
 import re
 import requests
 
-import project_functions 
-
 headers = {
     "api_key": st.secrets['api_key'],
     "content-type": "application/json"
@@ -31,88 +29,7 @@ st.markdown( f'<style>{css}</style>' , unsafe_allow_html= True)
 
 ### ---------------------------------------- FUNCTIONS   
 
-## A basic function to return resutls from API query to Companies House. 
-## Note the three required inputs. The last one can be None.
-def api_json(id_number, id_type, additional_information):
-        
-    base_url = f'https://api.companieshouse.gov.uk/{id_type}/{id_number}'
-    url = base_url if additional_information == None else base_url + '/' + additional_information        
-    response = requests.get(url, auth=(api_key, '')).json()
-    
-    return response
-
-
-## This function returns a dataframe containing all of a companies Officers and Persons With Significant Control.
-def get_directors(company_number):
-
-    df_directors = pd.DataFrame()
-
-    json_company = api_json(company_number, 'company', None)
-
-    print(json_company)
-    
-    api_person_types = ['officers', 'persons-with-significant-control']
-
-    for person_type in api_person_types:
-        json_data = api_json(company_number, 'company', person_type)
-        df_json_data = pd.json_normalize(json_data['items'])
-        df_directors = pd.concat([df_directors, df_json_data]).reset_index(drop = True)
-    
-    df_directors.insert(0, 'company_name', json_company['company_name'])
-    df_directors.insert(1, 'company_number', json_company['company_number'])
-    df_directors.insert(2, 'company_status', json_company['company_status'])
-    df_directors.insert(3, 'date_of_creation', json_company['date_of_creation'])
-    
-    return df_directors
-
-
-## A function that returns a dataframe listing a director's appointments. 
-## Note that it only shows that first xxx appointments, so for institutional investors, it will be incomplete
-
-def get_directors_appointments(director_id):
-
-    df_director_appointments = pd.DataFrame()
-
-    json_appointments = api_json(director_id, 'officers', 'appointments')
-    
-    ## For those with more than 1 page of results
-    total_appointments = json_appointments['total_results']
-    if total_appointments > json_appointments['items_per_page']:
-        json_appointments = api_json(director_id, 'officers', f'appointments?items_per_page={total_appointments}')
-    
-    number_of_appointments = len(json_appointments['items'])
-    for a in range(0, number_of_appointments):
-
-        appointment_data = json_appointments['items'][a]
-        df_a = pd.json_normalize(appointment_data)
-        df_director_appointments = pd.concat([df_director_appointments, df_a]).reset_index(drop = True)
-
-    return df_director_appointments
-
-
-## A function to collate the appointments of all people of a company's Officers and PSCs.
-def get_companyDirectorAppointments(df_directors):
-    
-    director_ids = [x.split('/')[-2] for x in df_directors['links.officer.appointments'] if pd.notna(x)]
-    
-    df_company_director_appointments = pd.DataFrame()
-    
-    for director_id in director_ids:
-                    
-        df_da = get_directors_appointments(director_id)
-        df_company_director_appointments = pd.concat([df_company_director_appointments, df_da]).reset_index(drop = True)
-        
-    for col in [x for x in df_company_director_appointments if x.startswith('appointed_to.')]:
-        df_company_director_appointments = df_company_director_appointments.rename(columns = {col: col.replace('appointed_to.', '').strip()})
-    
-    
-    df_company_director_appointments = pd.concat([df_directors, df_company_director_appointments])
-    df_company_director_appointments = df_company_director_appointments.reset_index(drop = True)
-    
-    return df_company_director_appointments
-        
-    
-### Plotting function contained in project_functions.py
+import project_functions 
     
 
 ### --------------------------------------- RUN 
